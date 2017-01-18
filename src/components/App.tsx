61f61cd249a90c4e57aeadc156f2ed1cb1adfcba
Gabriel Bull <me@gabrielbull.com>
2017-01-18 10:27:51 -0500

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => (
+  <div className={'app'}>
+    <h2>Hello, World</h2>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+var webpack = require('webpack');
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+
+var PROD = !(process.env.NODE_ENV === 'development');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    './index.tsx'
+  ],
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+
+  context: path.resolve(__dirname, 'src'),
+
+  devtool: (!PROD ? 'inline-source-map' : null),
+
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname),
+    publicPath: '/'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader!ts-loader'
+      },
+      {
+        test: /\.scss|\.css$/,
+        loader: 'style-loader!' + 'css-loader!' + 'postcss-loader!' + 'sass-loader'
+      },
+      {
+        test: /\.pug|\.jade/,
+        loader: 'json-loader!pug-loader'
+      },
+      {
+        test: /\.yml|\.yaml/,
+        loader: 'json-loader!yaml-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: function () {
+          return [autoprefixer];
+        }
+      }
+    })
+  ]
+};
